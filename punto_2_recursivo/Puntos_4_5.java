ab964bde9d655b3e6379f65e31b279fb1156036b
JimmyAlejo05 <50591094+JimmyAlejo05@users.noreply.github.com>
2020-04-22 07:56:02 -0500

    Update Puntos_4_5.java

--- punto_2_recursivo/Puntos_4_5.java
+++ punto_2_recursivo/Puntos_4_5.java
@@ -14,11 +14,11 @@ import usb.taller4.punto_2_recursivo.controlador.ControladorPunto5;
  *
  * @author jimmy rios
  */
-public class Puntos_4_5 {
+public class Puntos_4 {
 
     public static void main(String[] args) throws IOException {
         punto4();
-        punto5();
+       
     }
 
     static void punto4() throws IOException {
@@ -30,65 +30,5 @@ public class Puntos_4_5 {
         contadores.contadorConsonantes(cadena, 0, 0);
         contadores.contarEspacios(cadena, 0, 0);
         contadores.contarPalabras(cadena, 0);
-    }
-
-    static void punto5() {
-        ControladorPunto5 torreA = new ControladorPunto5();
-        torreA.push(4);
-        torreA.push(3);
-        torreA.push(2);
-        torreA.push(1);
-        ControladorPunto5 torreB = new ControladorPunto5();
-        ControladorPunto5 torreC = new ControladorPunto5();
-        boolean continuar = true;
-        while (continuar) {
-            torreA.imprimir();
-            torreB.imprimir();
-            torreC.imprimir();
-            System.out.println("Extraer de la torre:");
-            Scanner sc = new Scanner(System.in);
-            String torre = sc.nextLine();
-            String SegundoPaso;
-            switch (torre) {
-                case "A":
-                    System.out.println("Mover a la torre: ");
-                    SegundoPaso = sc.nextLine();
-                    switch (SegundoPaso) {
-                        case "B":
-                            torreB.apilar(torreA.desapilar());
-                            break;
-                        case "C":
-                            torreC.apilar(torreA.desapilar());
-                            break;
-                    }
-                    break;
-                case "B":
-                    System.out.println("Mover a la torre: ");
-                    SegundoPaso = sc.nextLine();
-                    switch (SegundoPaso) {
-                        case "A":
-                            torreA.apilar(torreB.desapilar());
-                            break;
-                        case "C":
-                            torreC.apilar(torreB.desapilar());
-                            break;
-                    }
-                    break;
-                case "C":
-                    System.out.println("Mover a la torre: ");
-                    SegundoPaso = sc.nextLine();
-                    switch (SegundoPaso) {
-                        case "A":
-                            torreA.apilar(torreC.desapilar());
-                            break;
-                        case "B":
-                            torreB.apilar(torreC.desapilar());
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
     }
 }
